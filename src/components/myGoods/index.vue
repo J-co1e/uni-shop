0738ef4fed325e265a04f17f72f853c05c4a794b
J-co1e <1310679387@qq.com>
2022-10-31 17:37:39 +0800

    完成商品列表页面

---
+++ src/components/myGoods/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <view>
+    <view class="goodsItem">
+				<view class="itemLeft">
+					<image :src="item.goods_small_logo || '../../static/logo.png'" />
+				</view>
+				<view class="itemRight">
+					<view>{{item.goods_name}}</view>
+					<view>￥{{item.goods_price}}</view>
+				</view>
+			</view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "myGoods",
+  props: {
+    item:{
+      type:Object,
+      default:{}
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {
+  },
+  watch: {},
+
+  // 组件周期函数--监听组件挂载完毕
+  mounted() {},
+  // 组件周期函数--监听组件数据更新之前
+  beforeUpdate() {},
+  // 组件周期函数--监听组件数据更新之后
+  updated() {},
+  // 组件周期函数--监听组件激活(显示)
+  activated() {},
+  // 组件周期函数--监听组件停用(隐藏)
+  deactivated() {},
+  // 组件周期函数--监听组件销毁之前
+  beforeDestroy() {},
+}
+</script>
+
+<style scoped lang="less">
+.goodsItem {
+	display: flex;
+	font-size: 13.5px;
+	border-bottom: 1px solid #ccc;
+	box-shadow: 0 1px 1px #aaa;
+	padding: 7px 5px;
+	.itemLeft {
+		margin-right: 5px;
+		image {
+			width: 100px;
+			height: 100px;
+			display: block;
+		}
+	}
+	.itemRight {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		:nth-last-child(1) {
+			font-size: 16px;
+			color: #c00000;
+		}
+	}
+}
+</style>
--- src/sub_pkg/goods_list/index.vue
+++ src/sub_pkg/goods_list/index.vue
@@ -1,38 +1,86 @@
 <template>
-  <div class="goods_list">goods_list</div>
+	<view class="cont">
+		<view v-for="(item,i) in queryObj.goodsList" :key="i" @click="toDetail(item)">
+			<my-goods :item="item"></my-goods>
+		</view>
+ 	</view>
 </template>
 
 <script>
+import myGoods from '../../components/myGoods/index.vue'
 export default {
-  name: "goods_list",
-  components: {},
-  props: {},
-  data() {
-    return {}
-  },
-  computed: {},
-  methods: {},
-  watch: {},
+	name: "goods_list",
+	components: {myGoods},
+	props: {},
+	data() {
+		return {
+			queryObj: {
+				query: "",
+				cid: "",
+				pagenum: 1,
+				pagesize: 10,
+				isLoading: false,
+				goodsList: [],
+				total: 0,
+			},
+		};
+	},
+	computed: {},
+	methods: {
+		async getGoodsList(cb) {
+      this.isLoading=true
+			let { data: res } = await uni.$http.get(
+				"/api/public/v1/goods/search",
+				this.queryObj
+			);
+      this.isLoading=false
+      cb&&cb()
+			if (res.meta.status !== 200) return uni.$showMsg();
+			this.queryObj.goodsList = [...this.queryObj.goodsList,...res.message.goods]
+			this.queryObj.total = res.message.total;
+		},
+    toDetail(item){
+      uni.navigateTo({ url: `/sub_pkg/goods_detail/index?goods_id=${item.goods_id}` })
+    }
+	},
+	watch: {},
 
-  // 页面周期函数--监听页面加载
-  onLoad() {},
-  // 页面周期函数--监听页面初次渲染完成
-  onReady() {},
-  // 页面周期函数--监听页面显示(not-nvue)
-  onShow() {},
-  // 页面周期函数--监听页面隐藏
-  onHide() {},
-  // 页面周期函数--监听页面卸载
-  onUnload() {},
-  // 页面处理函数--监听用户下拉动作
-  // onPullDownRefresh() { uni.stopPullDownRefresh(); },
-  // 页面处理函数--监听用户上拉触底
-  // onReachBottom() {},
-  // 页面处理函数--监听页面滚动(not-nvue)
-  // onPageScroll(event) {},
-  // 页面处理函数--用户点击右上角分享
-  // onShareAppMessage(options) {},
-} 
+	// 页面周期函数--监听页面加载
+	onLoad(options) {
+		this.getGoodsList();
+		this.queryObj.query = options.query || "";
+		this.queryObj.cid = options.cid || "";
+	},
+	// 页面周期函数--监听页面初次渲染完成
+	onReady() {},
+	// 页面周期函数--监听页面显示(not-nvue)
+	onShow() {},
+	// 页面周期函数--监听页面隐藏
+	onHide() {},
+	// 页面周期函数--监听页面卸载
+	onUnload() {},
+	// 页面处理函数--监听用户下拉动作
+	onPullDownRefresh() {
+    this.queryObj.pagenum=1
+    this.queryObj.total=0
+    this.isLoading=false
+    this.queryObj.goodsList=[]
+    this.getGoodsList(()=>uni.stopPullDownRefresh())
+	},
+	// 页面处理函数--监听用户上拉触底
+	onReachBottom() {
+    if(this.queryObj.pagenum*this.queryObj.pagesize>=this.queryObj.total) return uni.$showMsg('数据加载完毕')
+    if(this.isLoading) return
+    this.queryObj.pagenum+=1
+    this.getGoodsList()
+  },
+	// 页面处理函数--监听页面滚动(not-nvue)
+	// onPageScroll(event) {},
+	// 页面处理函数--用户点击右上角分享
+	// onShareAppMessage(options) {},
+};
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+
+</style>
